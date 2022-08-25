2e1cd4004fb0d8def6eb4b503f9ba651c1d1e452
Rose Maina <rosewairimu27@gmail.com>
2022-08-25 14:52:05 +0300

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,12 @@
 require 'sqlite3'
 
-
+#Creating the database, 
 DB = {:conn => SQLite3::Database.new("db/songs.db")}
+
+#Droppig songs to avoid an error.
 DB[:conn].execute("DROP TABLE IF EXISTS songs")
 
+#Creating the songs table.
 sql = <<-SQL
   CREATE TABLE IF NOT EXISTS songs (
   id INTEGER PRIMARY KEY,
@@ -13,4 +16,8 @@ sql = <<-SQL
 SQL
 
 DB[:conn].execute(sql)
+#Using the results_as_hash method availed by the SQLite3-Ruby gem. 
+#This method says: when a SELECT statement is executed, don't return a database row as an array, 
+#return it as a hash with the column names as keys.
+
 DB[:conn].results_as_hash = true
--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,9 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
+#Using the table_name method referenced by the self keyword, 
+#for grabbing the table_name that will be used in querying for column names.
 class Song
-
-
   def self.table_name
     self.to_s.downcase.pluralize
   end
@@ -11,20 +11,31 @@ class Song
   def self.column_names
     DB[:conn].results_as_hash = true
 
+#PRAGMA  is used for returning column names as an array of hashes that describe the table itself
+# Every hash has info about one column
+# Here, the SQL statement uses the pragma keyword and the #table_name method for accessing the name of table being queried
+# An iteration over resulting array of hashes is done to collect only the name of every column. 
     sql = "pragma table_info('#{table_name}')"
 
     table_info = DB[:conn].execute(sql)
     column_names = []
+    
     table_info.each do |row|
       column_names << row["name"]
     end
+    
     column_names.compact
   end
+
+#Iterating through the column names stored in the column_names class method
+#The settting an attr_accessor for each column and convertig each to a symbol using the to_sym method.
 
   self.column_names.each do |col_name|
     attr_accessor col_name.to_sym
   end
 
+  #Defining  the #initialize method to take in an argument of options
+  #send method to interpolate the name of each hash key as a method that is equal to the key's value.
   def initialize(options={})
     options.each do |property, value|
       self.send("#{property}=", value)
@@ -37,10 +48,12 @@ class Song
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
 
+#Abstractig the table name
   def table_name_for_insert
     self.class.table_name
   end
 
+#Abstracting the colum names
   def values_for_insert
     values = []
     self.class.column_names.each do |col_name|
